ca9aed3f66eee3f4ad172387676cf02c8e86b2fc
Amman Kamili <akamili2@charlotte.edu>
2025-11-15 00:14:10 -0500

    AccountFactory.cs and IAccount.cs completed.

---
+++ AccountFactory.cs
@@ -0,0 +1,36 @@
+﻿namespace BoulderDash;
+/*should totalAccount be all accounts the factory creates or all accounts in the program?
+If we do store accounts in a txt file or smth, I don't think totalAccount should be in here 
+Ill come back to this and update AccountFactory based on what yall think.*/
+public static class AccountFactory
+{
+    /*initialize id counter at 1 so when a user
+     creates an account in CreateAccount, its assigned 1 and 
+     incremented for new users.*/
+    private int _nextId = 1;
+    
+
+    //private object class that implements IAccount validating factory 
+    private class Account : IAccount
+    {
+        public string Username { get; set; }
+        public int Id { get; set; }
+
+        public Account(string username, int id)
+        {
+            Username = username;
+            Id = id;
+        }
+    }
+
+    public IAccount CreateAccount(string username)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+            throw new ArgumentException("Username cannot be null or whitespace.", nameof(username));
+
+        int id = _nextId++;
+        return new Account(username, id); // returns as IAccount
+    }
+}
+}
+}
---
+++ IAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace BoulderDash;
+
+public interface IAccount {
+    // Username for an account
+    string Username { get; set; }
+
+    // ID for an account
+    int Id { get; set; }
+}
