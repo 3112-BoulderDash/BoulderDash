3fbe5eb665ae0fad6d112e4bd9eac4251adc43e0
Amman Kamili <akamili2@charlotte.edu>
2025-12-02 11:16:17 -0500

    demo prep

--- Program.cs
+++ Program.cs
@@ -17,12 +17,12 @@ class Program
         GameField gameField = GameField.GetGameInstance();
         Controller playerController = new Controller();
         // main menu will go here
-        //RunMainMenu();
+        RunMainMenu();
         gameField.StartGame(playerController);
-
+        TempScoreLoggingDemo(leaderBoard, currentPlayer);
         // temporary section - when bryan implements the health functionality
         // and how the game ends, all temp should change
-        //TempScoreLoggingDemo(leaderBoard, currentPlayer);
+        
 
         while (gameField.GameIsRunning())
         {
@@ -88,7 +88,7 @@ class Program
     }
 
     // temp score logging, logic should stay similar once end to game loop is created.
-   /* private static void TempScoreLoggingDemo(LeaderBoard leaderBoard, IAccount currentPlayer)
+    private static void TempScoreLoggingDemo(LeaderBoard leaderBoard, IAccount currentPlayer)
     {
         Console.Clear();
         Console.WriteLine($"Logged in as: {currentPlayer.Username} (ID {currentPlayer.Id})");
@@ -113,9 +113,10 @@ class Program
         }
 
         Console.WriteLine("Press any key to start the game...");
-        Console.ReadKey(true); */
-   
-   //main menu
+        Console.ReadKey(true);
+    }
+
+    //main menu
    public static void RunMainMenu()
     {
         bool running = true;
