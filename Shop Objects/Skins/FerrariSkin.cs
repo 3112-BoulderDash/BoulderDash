370c4cddc9bc45e1f5a162ddd6c4da53bac4a32c
Fong Vang <118277142+fongv17@users.noreply.github.com>
2025-12-07 18:33:27 -0500

    changed skin prices

--- Shop Objects/Skins/FerrariSkin.cs
+++ Shop Objects/Skins/FerrariSkin.cs
@@ -3,7 +3,7 @@ namespace Boulder_Dash;
 public class FerrariSkin : ISkin
 {
     public string name { get; } = "Ferrari";
-    public int price { get; } = 300;
+    public int price { get; } = 5000;
     public string[] Sprite => new[]
     {
         "[*F*]",
--- Shop Objects/Skins/WheelSkin.cs
+++ Shop Objects/Skins/WheelSkin.cs
@@ -3,7 +3,7 @@ namespace Boulder_Dash;
 public class WheelSkin : ISkin
 {
     public string name { get; } = "Wheel";
-    public int price { get; } = 100;
+    public int price { get; } = 1000;
     public string[] Sprite => new[]
     {
         "     ",
